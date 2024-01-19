f8f09a54d743ed6623ac15b54b68ae184fa3a7e1
whoisxanny <tuffx4nny@gmail.com>
2024-01-19 17:28:49 +0500

    Initials

---
+++ src/main/java/pro/sky/booksshop/BooksShopApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.booksshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksShopApplication.class, args);
+	}
+
+}
